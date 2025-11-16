653c9cc4b697a10d9aa32c66fdb2a02574748d33
Naveen Bhardwaj <nav16011991@gmail.com>
2025-11-16 21:27:41 +0400

    Initial set up completed, virtual environment setup completed.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlops-vehicle-insaurance",
+    version="0.1.0",
+    author="Naveen Bhardwaj",
+    author_email="",
+    description="A project for predicting vehicle insurance premiums",
+    packages=find_packages(where="src")
+
+)
